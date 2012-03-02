884ad42887e793279bae1a2b780dc7d30ef98ded
harj <harjeet.taggar@gmail.com>
2012-03-01 19:46:00 -0800

    trying to figure out pi

--- Calculator/Calculator/CalculatorBrain.m
+++ Calculator/Calculator/CalculatorBrain.m
@@ -17,9 +17,7 @@
 
 -(NSMutableArray *) operandStack
 {
-    if (_operandStack == nil) {
-        _operandStack = [[NSMutableArray alloc] init];
-    }
+    if (_operandStack == nil) _operandStack = [[NSMutableArray alloc] init];
     return _operandStack;
 }
 
@@ -29,9 +27,33 @@
     
 }
 
+-(double)popOperand
+{
+    NSNumber *operandObject = [self.operandStack lastObject];
+    if (operandObject) [self.operandStack removeLastObject];
+    return [operandObject doubleValue];
+}
+
+
 -(double)performOperation:(NSString *)operation
 {
     double result = 0;
+    
+    if ([operation isEqualToString:@"+"]) {
+        result = [self popOperand] + [self popOperand];
+    } else if ([@"*" isEqualToString:operation]) {
+        result = [self popOperand] * [self popOperand];
+    } else if ([@"/" isEqualToString:operation]) {
+        result = [self popOperand] / [self popOperand];
+    } else if ([@"-" isEqualToString:operation]) {
+        result = [self popOperand] - [self popOperand];
+    } else if ([@"sin" isEqualToString:operation]) {
+        result = sin([self popOperand]);
+    } else if ([@"π" isEqualToString:operation]) {
+        
+    }
+    
+    [self pushOperand:result];
     return result;   
 }
 
--- Calculator/Calculator/CalculatorViewController.m
+++ Calculator/Calculator/CalculatorViewController.m
@@ -37,16 +37,17 @@
     
 }
 
+- (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+}
+
 - (IBAction)operationPressed:(UIButton *)sender 
 {
-    if (self.userIsInTheMiddleOfEnteringANumber) { [self enterPressed] };
+    if (self.userIsInTheMiddleOfEnteringANumber) [self enterPressed];
     double result = [self.brain performOperation:sender.currentTitle];
     NSString *resultString = [NSString stringWithFormat:@"%g", result];
     self.display.text = resultString;
 }
 
-- (IBAction)enterPressed {
-    [self.brain pushOperand:[self.display.text doubleValue]];
-    self.userIsInTheMiddleOfEnteringANumber = NO;
-}
 @end
